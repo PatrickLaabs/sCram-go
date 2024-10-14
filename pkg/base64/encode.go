71fe75efe48f0bf7e205f6757e2d1b55e536a9a0
PatrickLaabs <patrick.laabs@me.com>
2024-10-14 09:56:21 +0200

    some fine-tuning

--- pkg/base64/encode.go
+++ pkg/base64/encode.go
@@ -5,5 +5,6 @@ import "encoding/base64"
 func Encode(src []byte) (dest []byte) {
 	dest = make([]byte, base64.StdEncoding.EncodedLen(len(src)))
 	base64.StdEncoding.Encode(dest, src)
+
 	return dest
 }
--- pkg/sha256/sum.go
+++ pkg/sha256/sum.go
@@ -1,9 +1,17 @@
 package sha256
 
-import "crypto/sha256"
+import (
+	"crypto/sha256"
+	"fmt"
+)
 
 func Sum(key []byte) []byte {
 	h := sha256.New()
-	_, _ = h.Write(key)
+
+	_, err := h.Write(key)
+	if err != nil {
+		fmt.Printf("Error writing sha256 key %v\n", err)
+	}
+
 	return h.Sum(nil)
 }
